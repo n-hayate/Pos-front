b9167bc6a76e0db047ac8ef7879a1086b33087c3
n-hayate <naohtcb.streinten.for.kiminozo@gmail.com>
2025-10-13 21:25:51 +0900

    5回コミット

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 // next.config.js
-require('dotenv').config();
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'standalone',
   reactStrictMode: false,
-
-  // /next/* で来たものは /_next/* に繋ぎ替える
   async rewrites() {
+    // Azureなどで /_next が /next に変換されるケース対策
     return [{ source: '/next/:path*', destination: '/_next/:path*' }];
   },
-
-  // 必要に応じて配信プレフィックスを環境変数で切り替え可能に
-  assetPrefix: process.env.NEXT_ASSET_PREFIX || undefined,
 };
-
-module.exports = nextConfig;
