2a399d75e21d382e116023d51551f0c620f1a723
n-hayate <naohtcb.streinten.for.kiminozo@gmail.com>
2025-10-14 20:36:46 +0900

    10回コミット

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-// next.config.js
-module.exports = {
-  output: 'standalone',
-  reactStrictMode: false,
-  async rewrites() {
-    // 万一、上流で /_next → /next に変換されても拾えるように
-    return [{ source: '/next/:path*', destination: '/_next/:path*' }];
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone'
+      }
+
+module.exports = nextConfig
